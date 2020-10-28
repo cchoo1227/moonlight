6dfb2f743abbe94c7b5e566a5b90613ba5e99d2c
cchoo1227 <64781072+cchoo1227@users.noreply.github.com>
2020-10-28 16:37:46 +0800

    Update bookings.sql

--- bookings.sql
+++ bookings.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 28, 2020 at 02:47 PM
+-- Generation Time: Oct 28, 2020 at 04:14 PM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.11
 
@@ -28,12 +28,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `bookings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ticketType` int(11) NOT NULL,
-  `ticketPrice` int(11) NOT NULL,
+  `ticketType` varchar(20) NOT NULL,
+  `ticketPrice` float NOT NULL,
   `screeningId` int(11) NOT NULL,
   `seats` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `ticketType`, `ticketPrice`, `screeningId`, `seats`) VALUES
+(1, 'Adult', 10, 1, 'A1, A2, A3'),
+(2, 'Adult', 10, 2, 'A1, A2, A3'),
+(3, 'Student', 6, 3, 'A1, A2, A3');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
